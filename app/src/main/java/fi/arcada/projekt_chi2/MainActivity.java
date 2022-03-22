d705da37a1cdac156e503a6c037c8eeef3cd8ea3
Fredrik Welander <fredrik.welander@subsite.fi>
2022-03-22 19:15:43 +0200

    Add starter code

--- app/src/main/java/fi/arcada/projekt_chi2/MainActivity.java
+++ app/src/main/java/fi/arcada/projekt_chi2/MainActivity.java
@@ -3,12 +3,80 @@ package fi.arcada.projekt_chi2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Deklarera 4 Button-objekt
+    Button btn1, btn2, btn3, btn4;
+    // Deklarera 4 heltalsvariabler för knapparnas värden
+    int val1, val2, val3, val4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Koppla samman Button-objekten med knapparna i layouten
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
+
+
     }
+
+    /**
+     *  Klickhanterare för knapparna
+     */
+    public void buttonClick(View view) {
+
+        // Skapa ett Button-objekt genom att type-casta (byta datatyp)
+        // på det View-objekt som kommer med knapptrycket
+        Button btn = (Button) view;
+
+        // Kontrollera vilken knapp som klickats, öka värde på rätt vaiabel
+        if (view.getId() == R.id.button1) val1++;
+        if (view.getId() == R.id.button2) val2++;
+        if (view.getId() == R.id.button3) val3++;
+        if (view.getId() == R.id.button4) val4++;
+
+        // Slutligen, kör metoden som ska räkna ut allt!
+        calculate();
+    }
+
+    /**
+     * Metod som uppdaterar layouten och räknar ut själva analysen.
+     */
+    public void calculate() {
+
+        // Uppdatera knapparna med de nuvarande värdena
+        btn1.setText(String.valueOf(val1));
+        btn2.setText(String.valueOf(val2));
+        btn3.setText(String.valueOf(val3));
+        btn4.setText(String.valueOf(val4));
+
+        // Mata in värdena i Chi-2-uträkningen och ta emot resultatet
+        // i en Double-variabel
+        double chi2 = Significance.chiSquared(val1, val2, val3, val4);
+
+        // Mata in chi2-resultatet i getP() och ta emot p-värdet
+        double pValue = Significance.getP(chi2);
+
+        /**
+         *  - Visa chi2 och pValue åt användaren på ett bra och tydligt sätt!
+         *
+         *  - Visa procentuella andelen jakande svar inom de olika grupperna.
+         *    T.ex. (val1 / (val1+val3) * 100) och (val2 / (val2+val4) * 100
+         *
+         *  - Analysera signifikansen genom att jämföra p-värdet
+         *    med signifikansnivån, visa reultatet åt användaren
+         *
+         */
+
+    }
+
+
 }
---
+++ app/src/main/java/fi/arcada/projekt_chi2/Significance.java
@@ -0,0 +1,55 @@
+package fi.arcada.projekt_chi2;
+
+public class Significance {
+
+    /**
+     * Metod som räknar ut Chi-två på basis av fyra observerade värden (o1 - o4).
+     */
+    public static double chiSquared(int o1, int o2, int o3, int o4) {
+
+        // heltalsvariabler tänkta att få de förväntade värdena
+        int e1, e2, e3, e4;
+
+        /**
+         *  Implementera din egen Chi-två-uträkning här!
+         *
+         *  1.  Räkna de förväntade värdena, spara resultaten i e1 - e4
+         *
+         *  2.  Använd de observerade värdena (o1 - o4) och de förväntade
+         *      värdena (e1 - e4) för att räkna ut Chi-två enligt formeln.
+         *
+         *  3.  returnera resultatet
+         *      (använd det sedan för att få p-värdet via getP()
+         *
+         * */
+
+        return 0.0;
+    }
+
+
+    /**
+     * Metod som tar emot resultatet från Chi-två-uträkningen
+     * och returnerar p-värde enligt tabellen (en frihetsgrad)
+     * (De mest extrema värdena har lämnats bort, det är ok för våra syften)
+     *
+     * exempel: getP(2.82) returnerar ett p-värde på 0.1
+     *
+     */
+    public static double getP(double chiResult) {
+
+        double p = 0.99;
+
+        if (chiResult >= 1.642) p = 0.2;
+        if (chiResult >= 2.706) p = 0.1;
+        if (chiResult >= 3.841) p = 0.05;
+        if (chiResult >= 5.024) p = 0.025;
+        if (chiResult >= 5.412) p = 0.02;
+        if (chiResult >= 6.635) p = 0.01;
+        if (chiResult >= 7.879) p = 0.005;
+        if (chiResult >= 9.550) p = 0.002;
+
+        return p;
+
+    }
+
+}
